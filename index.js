b037fd8f02e07edceaeaa1695bfd119e73d40b2e
Suchitra Naik <susanaik@gmail.com>
2018-07-09 15:22:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
 function deleteFromDriverByKey(driver, key){
 
 }
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+
+}
