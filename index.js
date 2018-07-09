dfb059d21ba83439dc4e0ba094bd1006fcccf3a9
Suchitra Naik <susanaik@gmail.com>
2018-07-09 15:23:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value){
-  driver[] = value;
+  driver.key = value;
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
